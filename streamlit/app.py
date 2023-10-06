a3a8ea73363d5c9a561b7d725b7b6d6ffc358a64
Harsh Shah <shah.harsh7@northeastern.edu>
2023-10-05 23:53:19 -0400

    git initial setup with homepage

---
+++ streamlit/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from auth_user import create_user,login_user
+
+st.set_page_config(page_title="BiteBuddy", layout="wide")
+with st.sidebar:
+    # options menu
+    selected = st.selectbox("Menu", ["Log In", 'Sign Up'])
+    
+    # log in form
+    if 'login' not in st.session_state:
+        st.session_state['login'] = False
+
+
+
+    if selected == "Log In":
+        
+        st.write('## Log In')
+        login_username = st.text_input('Email')
+        login_password = st.text_input('Password', type='password')
+        # authentication status update
+        if st.button('Log In!'):
+            # send login request
+            st.session_state['login'] = login_user(login_username,login_password)
+
+        if st.session_state['login'] == True:
+            if st.button("Logout"):
+                st.session_state['login'] = False
+
+    # # Sign-up form
+    if selected == "Sign Up":
+        st.write('## Sign up')
+        name = st.text_input('Name')
+        username = st.text_input('Email',key='signup_username')
+        password = st.text_input('Password', type='password',key='signup_pass')
+        confirm_password = st.text_input('Confirm Password', type='password')
+
+        
+        if st.button('Sign up'):
+            if password != confirm_password:
+                st.write("Passwords don't Match!")
+            else:
+                # send register request
+                signup_status = create_user(name,username,password)
+                if signup_status:
+                    st.success("User Registered Successfully! Sign-in to continue...")
+                else:
+                    st.error("Email already exists! Sign in to continue...")
+
+if  st.session_state['login'] != True:
+    st.title("Still Staring at the restuarant menu??😶")
+    st.image('images/3.webp', width=600)
+    st.write("""
+    Let us help... will you?
+    """)
+if  st.session_state['login'] == True:
+    st.title(" Welcome to your.... BITEBUDDY! 🍽️")
+    # st.image('1.', width=500)
+
